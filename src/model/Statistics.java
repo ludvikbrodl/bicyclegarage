65aaffb38e86367cf73a1c728e8d9d9a2d0f5737
Axel Martensson <dat12ama@hacke-3.student.lth.se>
2013-04-26 08:55:36 +0200

    added attributes for statistics class

--- src/model/Statistics.java
+++ src/model/Statistics.java
@@ -1,5 +1,18 @@
 package model;
 
+import java.util.ArrayList;
+
+import com.sun.org.apache.bcel.internal.generic.L2D;
+
 public class Statistics {
 	
+	private Date currentMonth;
+	private List<Integer> bicyclesPerMonth;
+	private int bicyclesInGarage;
+	private int entriesThisMonth;
+	
+	public Statistics() {
+		currentMonth = new Date();
+		bicyclesPerMonth = new ArrayList<Integer>();
+	}
 }
