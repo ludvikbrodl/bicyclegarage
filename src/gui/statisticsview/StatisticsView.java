19382ca7127293a365e4dba8f44b15d4f92f90c0
Axel Martensson <dat12ama@val-8.student.lth.se>
2013-04-29 13:14:48 +0200

    jscrollpane

--- src/gui/statisticsview/StatisticsView.java
+++ src/gui/statisticsview/StatisticsView.java
@@ -1,12 +1,32 @@
 package gui.statisticsview;
 import java.awt.Component;
+import java.awt.GridLayout;
+import java.util.List;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import persistence.Statistics;
 
 
 public class StatisticsView extends JPanel {
 	
-	public StatisticsView() {
+	public StatisticsView(Statistics statistics) {
 		super();
+		
+		setLayout(new GridLayout(2, 0));
+		int bicyclesInGarage = statistics.getBicycleInGarage();
+		int bicyclesTotal = statistics.getNumberOfBicycles();
+		int usersTotal = statistics.getNumberOfUsers();
+		List<Integer> entriesPerMonth = statistics.getNumberOfEntriesPerMonth();
+		int months = entriesPerMonth.size();
+		
+		JScrollPane labelPanel = new JScrollPane();
+		for(int entriesThisMonth : entriesPerMonth) {
+			labelPanel.add(new JLabel(Integer.toString(entriesThisMonth)));
+		}
+		add(labelPanel);
+		
 	}
 }
