543bbd7aa74c0d6e98c67fdd647f1e65e29bed40
Philip Pedersen <dat12ppe@val-7.student.lth.se>
2013-04-29 12:01:37 +0200

    Address implementerad
    Statistic påbörjad

--- src/model/Address.java
+++ src/model/Address.java
@@ -1,31 +1,55 @@
 package model;
-/**
-* Main-method used to start the application.
-* @param args
-*/
 
-class Address {  
-    public Address(String streetName, int streetNumber, int zipcode, String cityName) {
+/**
+ * Main-method used to start the application.
+ * 
+ * @param args
+ */
+
+class Address {
+	private int streetNumber;
+	private String streetName;
+	private int zipcode;
+	private String cityName;
+
+	public Address(String streetName, int streetNumber, int zipcode,
+			String cityName) {
+		this.streetNumber = streetNumber;
+		this.streetName = streetName;
+		this.zipcode = zipcode;
+		this.cityName = cityName;
+
 	}
-    
-    public String getStreetName() {
-		return null;
+
+	public String getStreetName() {
+		return streetName;
 	}
-    public void setStreetName() {
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
 	}
-    public int getStreetNumber() {
-		return 0;
+
+	public int getStreetNumber() {
+		return streetNumber;
 	}
-    public void setStreetNumber(int streetNumber) {
+
+	public void setStreetNumber(int streetNbr) {
+		streetNumber = streetNbr;
 	}
-    public int getZipcode() {
-		return 0;
+
+	public int getZipcode() {
+		return zipcode;
 	}
-    public void setZipcode(int zipcode) {
+
+	public void setZipcode(int zipcode) {
+
 	}
-    public String getCityName() {
-		return null;
+
+	public String getCityName() {
+		return cityName;
 	}
-    public void setCityName(String cityName) {
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
 	}
 }
--- src/persistence/Statistics.java
+++ src/persistence/Statistics.java
@@ -8,24 +8,38 @@ import com.sun.org.apache.bcel.internal.generic.L2D;
 
 public class Statistics {
 	
-	private Date currentMonth;
+	private Date date;
 	private List<Integer> bicyclesPerMonth;
 	private int bicyclesInGarage;
 	private int entriesThisMonth;
+	private Database DB;
+	private int lastMonth;
 	
 	public Statistics() {
-		currentMonth = new Date();
+		date = new Date();
 		bicyclesPerMonth = new ArrayList<Integer>();
+		DB = new Database();
 	}
+	
     public void incrementNumberOfBicyclesInGarage() {
+    	if(date.getDate() == 0 && lastMonth ==11){
+    		
+    	}
+ 
+    	
+    	bicyclesInGarage ++;
+    	
 	}
     public List<Integer> getNumberOfEntriesPerMonth() {
-		return null;
+		return bicyclesPerMonth;
 	}
     public int getNumberOfBicycles() {
-		return 0;
+    	return DB.getNumberOfBicycles();
 	}
     public int getNumberOfUsers() {
-		return 0;
+		return DB.getNumberOfUsers();
 	}
+    public int getBicycleInGarage(){
+    	return bicyclesInGarage;
+    }
 }
