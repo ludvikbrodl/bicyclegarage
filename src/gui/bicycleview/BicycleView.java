943c6d1d6a3952532f5d6683125788894d6708a2
Axel Martensson <dat12ama@val-8.student.lth.se>
2013-04-29 11:55:55 +0200

    Updated BicycleView

--- src/gui/bicycleview/BicycleView.java
+++ src/gui/bicycleview/BicycleView.java
@@ -2,28 +2,29 @@ package gui.bicycleview;
 import java.awt.Component;
 import java.awt.GridLayout;
 
+import model.BarcodePrinter;
 import model.Bicycle;
-
+import persistence.Database;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class BicycleView extends JPanel {
-	public BicycleView(Bicycle bicycle){
+	public BicycleView(String bicycleID, Database db, BarcodePrinter printer){
 				super();
 		setLayout(new GridLayout(2, 0));
 		
 		// Bicycle ID Panel
 		JPanel bicycleIDPanel = new JPanel();
 		JLabel bicycleIDEtiquette = new JLabel("Barcode:");
-		JLabel bicycleIDLabel = new JLabel(bicycle.getID());
+		JLabel bicycleIDLabel = new JLabel(bicycleID);
 		bicycleIDPanel.add(bicycleIDEtiquette);
 		bicycleIDPanel.add(bicycleIDLabel);
 		
 		// Button Panel
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(new JButtonPrint(bicycle.getID()));
-		buttonPanel.add(new JButtonRemoveBicycle(bicycle.getID()));
+		buttonPanel.add(new JButtonPrint(bicycleID, printer));
+		buttonPanel.add(new JButtonRemoveBicycle(bicycleID, db));
 		
 		
 		add(bicycleIDPanel);
--- src/gui/bicycleview/JButtonRemoveBicycle.java
+++ src/gui/bicycleview/JButtonRemoveBicycle.java
@@ -2,16 +2,20 @@ package gui.bicycleview;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.DatabaseMetaData;
 
 import javax.swing.JButton;
-
+import persistence.Database;
 public class JButtonRemoveBicycle extends JButton implements ActionListener {
-	public JButtonRemoveBicycle(String bicycleID){
+	private String bicycleID;
+	private Database db;
+	public JButtonRemoveBicycle(String bicycleID, Database db){
 		super("Remove Bicycle");
 		addActionListener(this);
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		System.out.println("Removing bicycle...");
+		db.
 	}
 }
