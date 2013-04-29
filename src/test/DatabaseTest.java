9448229d8f2b0f2d88e1fd4e6e0dde6756a50eb9
Axel Mårtensson <axel.martensson@hotmail.com>
2013-04-29 08:38:26 +0200

    Added test package
     * Added a package to hold all unit tests.
       to create a unit test for your class:
       Right click on class in eclipse -> new -> JUnit test case
       Make sure to change Package to test and to tick
       the checkboxes to generate setUp() and tearDown methods.
       Click next.
       Select all methods in the class and click finish.

---
+++ src/test/DatabaseTest.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author axel
+ *
+ */
+public class DatabaseTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#Database()}.
+	 */
+	@Test
+	public void testDatabase() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#hasBicycleWithID(java.lang.String)}.
+	 */
+	@Test
+	public void testHasBicycleWithID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#getBicycleByID(String)}.
+	 */
+	@Test
+	public void testGetBicycleByID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#updateBicycleID(String, Bicycle)}.
+	 */
+	@Test
+	public void testUpdateBicycleID() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#addBicycle(Bicycle)}.
+	 */
+	@Test
+	public void testAddBicycle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#removeBicycle(Bicycle)}.
+	 */
+	@Test
+	public void testRemoveBicycle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#hasUserWithPin(java.lang.String)}.
+	 */
+	@Test
+	public void testHasUserWithPin() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#getUserByName(String)}.
+	 */
+	@Test
+	public void testGetUserByName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#addUser(User)}.
+	 */
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#removeUser(User)}.
+	 */
+	@Test
+	public void testRemoveUser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#getNumberOfBicycles()}.
+	 */
+	@Test
+	public void testGetNumberOfBicycles() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link persistence.Database#getNumberOfUsers()}.
+	 */
+	@Test
+	public void testGetNumberOfUsers() {
+		fail("Not yet implemented");
+	}
+
+}
