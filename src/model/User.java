f399dc5b9a3faba776086b8149ec0ef8749431bd
Kasper HunkeN Bratz <dat12kbr@student.lu.se>
2013-04-29 12:03:36 +0200

    Implemented user
    Axel 4 president!

--- src/model/User.java
+++ src/model/User.java
@@ -1,43 +1,76 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
 * Main-method used to start the application.
 * @param args
 */
-
-class User {  
+public class User {  
+	
+	private String pincode;
+	private String name;
+	private String birthDate;
+	private Address address;
+	private List<Bicycle> bicycleList;
+	
         public User(String pincode, String name, String birthDate, Address address) {
+        	this. pincode = pincode;
+        	this.name = name;
+        	this.birthDate = birthDate;
+        	this.address = address;
+        	bicycleList = new ArrayList<Bicycle>();
 		}
+        
         public void addBicycle(Bicycle bicycle) {
+        	bicycleList.add(bicycle);
 		}
         public void removeBicycle(Bicycle bicycle) {
+        	bicycleList.remove(bicycleList.indexOf(bicycle));
 		}
         
         public List<String> getBicycleIDs() {
-			return null;
+        	List<String> tempList = new ArrayList<String>();
+        	Iterator<Bicycle> itr = bicycleList.iterator();
+        	Bicycle temp;
+        	
+        	while(itr.hasNext()){
+        		temp = (Bicycle) itr.next();
+        		tempList.add(temp.getID());
+        	}
+			return tempList;
 		}
+        
         public String getPincode() {
-			return null;
+			return pincode;
 		}
-        public String setPincode(String pincode) {
-			return null;
+        public void setPincode(String pincode) {
+        	this.pincode = pincode;
+   
 		}
         public String getName() {
-			return null;
+        	
+			return name;
 		}
         public void setName(String name) {
+        	this.name = name;
+        	
 		}
         public Address getAddress() {
-			return null;
+        	
+			return address;
 		}
-        public Address setAddress(Address address) {
-			return null;
+        
+        public void setAddress(Address address) {
+        	this.address = address;
 		}
         public String getBirthDate() {
-			return null;
+        	
+			return birthDate;
 		}
         public void setBirthDate() {
+        	this.birthDate = birthDate;
 		}
 }
