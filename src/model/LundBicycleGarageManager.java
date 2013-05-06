d65fa7a637afbc1a0b476547afd34ebaaa95faec
Axel Martensson <dat12ama@beta-3.student.lth.se>
2013-05-06 18:51:18 +0200

    added lightled on wrong pin- or barcode

--- src/model/LundBicycleGarageManager.java
+++ src/model/LundBicycleGarageManager.java
@@ -17,6 +17,7 @@ public class LundBicycleGarageManager implements BicycleGarageManager {
 	private Date lastCharacterEntryTime;
 	private StringBuilder pincode;
 	private Statistics statistics;
+	private final int MAX_BICYCLES_IN_GARAGE  = 2000;
 	private final int TIMEOUT = 15000;
 	private final char CLEAR_ALL = '*';
 	private final int OPEN_DOOR_TIME = 10;
@@ -41,10 +42,18 @@ public class LundBicycleGarageManager implements BicycleGarageManager {
 	@Override
 	public void entryBarcode(String bicycleID) {
 		if(db.hasBicycleWithID(bicycleID)) {
-			Bicycle bicycle = db.getBicycleByID(bicycleID);
-			entryLock.open(OPEN_DOOR_TIME);
-			bicycle.setInGarage(true);
-			statistics.incrementBicyclesInGarage();
+			if(statistics.getBicycleInGarage() < MAX_BICYCLES_IN_GARAGE) {
+				Bicycle bicycle = db.getBicycleByID(bicycleID);
+				entryLock.open(OPEN_DOOR_TIME);
+				bicycle.setInGarage(true);
+				statistics.incrementBicyclesInGarage();
+				
+			} else {
+				terminal.lightLED(PinCodeTerminal.RED_LED, 3);
+				terminal.lightLED(PinCodeTerminal.GREEN_LED, 3);
+			}
+		} else {
+			terminal.lightLED(PinCodeTerminal.RED_LED, 3);
 		}
 	}
 
@@ -55,7 +64,9 @@ public class LundBicycleGarageManager implements BicycleGarageManager {
 			exitLock.open(OPEN_DOOR_TIME);
 			bicycle.setInGarage(false);
 			statistics.decrementBicyclesInGarage();
-		}	
+		} else {
+			terminal.lightLED(PinCodeTerminal.RED_LED, 3);
+		}
 	}
 
 	@Override
@@ -71,10 +82,11 @@ public class LundBicycleGarageManager implements BicycleGarageManager {
 				if(db.hasUserWithPin(pincode.toString())) {
 					entryLock.open(OPEN_DOOR_TIME);
 				} else {
-					terminal.lightLED(0, 2);
+					terminal.lightLED(PinCodeTerminal.RED_LED, 3);
 				}
 			}
 		}
+		lastCharacterEntryTime = currentTime;
 	}
         
 }
