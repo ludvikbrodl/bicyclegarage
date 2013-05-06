968a04478b315ac6dcf4cf59c06b0b444bdd663d
Kasper HunkeN Bratz <dat12kbr@student.lu.se>
2013-05-06 15:50:15 +0200

    Started database. Lade till addID i bicycle. Test för user klar

--- src/persistence/Database.java
+++ src/persistence/Database.java
@@ -13,41 +13,61 @@ import model.User;
 */
 
 public class Database {  
+	
+	private final int MAX_USERS = 20000;
+	private Map<String, User> users;
 	private Map<String, Bicycle> bicycles;
+	private int nbrUsers;
+	
+	
         public Database() {
         	bicycles = new ConcurrentHashMap<String, Bicycle>();
+        	users = new ConcurrentHashMap<String, User>();
+        	nbrUsers = 0;
 		}
+        
         public boolean hasBicycleWithID(String bicycleID) {
 			return false;
 		}
+        
         public Bicycle getBicycleByID(String bicycleID) {
 			return bicycles.get(bicycleID);
 		}
-        public void updateBicycleID(String bicycleID, Bicycle bicycle) {
+        
+        public void updateBicycleID(String newBicycleID, String oldBicycleID) {
 		}
+        
         public void addBicycle(Bicycle bicycle) {
         	bicycles.put(bicycle.getID(), bicycle);
 		}
         public void removeBicycle(Bicycle bicycle) {
         	bicycles.remove(bicycle.getID());
 		}
+        
         public boolean hasUserWithPin(String pincode) {
 			return false;
 		}
+        
         public User getUserByName(String name) {
 			return null;
 		}
+        
         public void updateUserPincode() {
         }
+        
         public void addUser(User user) {
 		}
+        
         public void removeUser(User user) {
 		}
+        
         public int getNumberOfBicycles() {
 			return bicycles.size();
 		}
+        
         public int getNumberOfUsers() {
-			return 0;
+			return nbrUsers;
 		}
+        
         
 }
