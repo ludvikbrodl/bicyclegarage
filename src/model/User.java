a3af7f6b80fcb677a4bd4ef9c5d696e2468ca154
Axel Mårtensson <axel.martensson@hotmail.com>
2013-05-08 16:58:43 +0200

    Added doc comments

--- src/model/User.java
+++ src/model/User.java
@@ -8,8 +8,9 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
-* Main-method used to start the application.
-* @param args
+ * User describes a user of the bicycle garage.
+ * 
+ * @author bas12kbr
 */
 public class User implements Serializable {  
 	
@@ -27,7 +28,10 @@ public class User implements Serializable {
         	this.address = address;
         	bicycleList = new ArrayList<Bicycle>();
 		}
-        
+        /**
+         * Associates the specified Bicycle with the user.
+         * @param bicycle the bicycle to be added 
+         */
         public void addBicycle(Bicycle bicycle) {
         	if(bicycleList.size()>=5){
         		JOptionPane.showMessageDialog(null, "Max amount of bicycles has been registerd.");
@@ -36,12 +40,20 @@ public class User implements Serializable {
         	bicycleList.add(bicycle);
         	}
 		}
+        /**
+         * Removes the association between the specified bicycle and the user.
+         * 
+         * @param bicycle the bicycle to be removed
+         */
         public void removeBicycle(Bicycle bicycle) {
         	if(bicycleList.indexOf(bicycle)!= -1){
         	bicycleList.remove(bicycleList.indexOf(bicycle));
         	}
 		}
-        
+        /**
+         * Fetches a list of all the bicycles that the user is associated with
+         * @return list of bicycles, can be empty
+         */
         public List<String> getBicycleIDs() {
         	List<String> tempList = new ArrayList<String>();
         	Iterator<Bicycle> itr = bicycleList.iterator();
@@ -53,34 +65,73 @@ public class User implements Serializable {
         	}
 			return tempList;
 		}
-        
+        /**
+         * Fetches the user's pincode 
+         * @return pincode of the user
+         */
         public String getPincode() {
 			return pincode;
 		}
+        
+        /**
+         * Sets the user's pincode to the specified string.
+         * Notice that this method should not be called directly,
+         * use updateUserPincode in Database instead.
+         * 
+         * @param pincode the pincode to be set 
+         */
         public void setPincode(String pincode) {
         	this.pincode = pincode;
    
 		}
+        
+        /**
+         * Fetches the user's name 
+         * @return name of the user
+         */
         public String getName() {
         	
 			return name;
 		}
+        
+        /**
+         * Sets the user's name 
+         * @param name the name to be set
+         */
         public void setName(String name) {
         	this.name = name;
         	
 		}
+        
+        /**
+         * Fetches the user's address.
+         * @return address of the user
+         */
         public String getAddress() {
         	
 			return address;
 		}
         
+        /**
+         * Sets the user's address.
+         * @param address to be set 
+         */
         public void setAddress(String address) {
         	this.address = address;
 		}
+        
+        /**
+         * Fetches the user's birth date.
+         * @return birth date of the user
+         */
         public String getBirthDate() {
-        	
 			return birthDate;
 		}
+        
+        /**
+         * Sets the user's birth date.
+         * @param birthdate date to be set
+         */
         public void setBirthDate(String birthDate) {
         	this.birthDate = birthDate;
 		}
--- src/persistence/Database.java
+++ src/persistence/Database.java
@@ -15,9 +15,9 @@ import model.Bicycle;
 import model.User;
 
 /**
- * Main-method used to start the application.
+ * The database is the place where users and bicycles are stored.
  * 
- * @param args
+ * @author dat12kbr
  */
 
 public class Database implements Serializable{
@@ -27,22 +27,43 @@ public class Database implements Serializable{
 	private Map<String, User> users;
 	private Map<String, Bicycle> bicycles;
 
+	/**
+	 * Creates a new empty database.
+	 */
 	public Database() {
 		bicycles = new HashMap<String, Bicycle>();
 		users = new HashMap<String, User>();
 	}
 
+	/**
+	 * Tests if the database contains a bicycle with the specified id.
+	 * 
+	 * @param bicycleID the id to search for
+	 * @return true if the database contains a bicycle with id bicycleID
+	 */
 	public boolean hasBicycleWithID(String bicycleID) {
 		if (bicycles.containsKey(bicycleID)) {
 			return true;
 		}
 		return false;
 	}
-
+	
+	/**
+	 * Fetches the bicycle corresponding to the specified id
+	 * 
+	 * @param bicycleID the id of the bicycle to fetch 
+	 * @return Bicycle corresponding to bicycleID, or null if no bicycle was found
+	 */
 	public Bicycle getBicycleByID(String bicycleID) {
 		return bicycles.get(bicycleID);
 	}
 
+	/**
+	 * Creates a new bicycle, adds it to the database and to the specified user.
+	 * 
+	 * @param usr the user that the new bicycle should be associated with
+	 * @return the newly created bicycle
+	 */
 	public Bicycle newBicycle(User usr) {
 		String nbr = Integer.toString(BicycleID);
 		Bicycle toAdd = new Bicycle(nbr);
@@ -51,7 +72,13 @@ public class Database implements Serializable{
 		BicycleID++;
 		return toAdd;
 	}
-
+	/**
+	 * Removes the specified bicycle from the database.
+	 * CAUTION: this method does not dissociate the removed bicycle from its user,
+	 * it merely prevents the bicycle's id from being found when querying the database.
+	 * 
+	 * @param bicycle the bicycle to be removed from the database
+	 */
 	public void removeBicycle(Bicycle bicycle) {
 		bicycles.remove(bicycle.getID());
 	}
@@ -62,7 +89,14 @@ public class Database implements Serializable{
 		}
 		return false;
 	}
-
+	
+	/**
+	 * Fetches the user with the specified name.
+	 * 
+	 * @param name the name of the user
+	 * @return User with the specified name, null if it is not found
+	 * 
+	 */
 	public User getUserByName(String name) {
 		Iterator<User> itr = users.values().iterator();
 		while (itr.hasNext()) {
@@ -73,7 +107,15 @@ public class Database implements Serializable{
 		}
 		return null;
 	}
-
+	/**
+	 * Changes the pincode of the user associated with the old pincode
+	 * to the new pincode.
+	 * Notice that this method should be used instead of calling
+	 * user.setPincode() direcly, as this will corrupt the data in the database.
+	 * 
+	 * @param newPincode the new pincode
+	 * @param oldPincode the old pincode
+	 */
 	public void updateUserPincode(String newPincode, String oldPincode) {
 		if (users.containsKey(oldPincode) && !newPincode.equals(oldPincode)) {
 			User user = users.remove(oldPincode);
@@ -90,7 +132,13 @@ public class Database implements Serializable{
 					"No such pincode exists");
 		}
 	}
-
+	
+	/**
+	 * Adds the specified user to the database.
+	 * 
+	 * @param user the user to be added
+	 * @throws UserLimitException if the maximum limit of users in the database is reached
+	 */
 	public void addUser(User user) throws UserLimitException{
 		if (getNumberOfUsers() < MAX_USERS) {
 			users.put(user.getPincode(), user);
@@ -98,19 +146,34 @@ public class Database implements Serializable{
 			throw new UserLimitException();
 		}
 	}
-
+	/**
+	 * Removes the specified user from the database. 
+	 * Notice that it does not
+	 * dissociate the bicycles associated with the user from the database.
+	 * 
+	 * @param user the user to be removed
+	 */
 	public void removeUser(User user) {
 		users.remove(user.getPincode());
 	}
-
+	/**
+	 * Fetches the number of bicycles stored in the database.
+	 * @return the number of bicycles in the database 
+	 */
 	public int getNumberOfBicycles() {
 		return bicycles.size();
 	}
 
+	/**
+	 * Fetches the number of users stored in the database.
+	 * @return the number of bicycles in the database 
+	 */
 	public int getNumberOfUsers() {
 		return users.size();
 	}
-
+	/**
+	 * Writes the contents of the database to the harddrive.
+	 */
 	public void saveToFile() {
 		try {
 			ObjectOutputStream out = new ObjectOutputStream(
@@ -137,6 +200,9 @@ public class Database implements Serializable{
 		}
 	}
 
+	/**
+	 * Retrieves the contents of the database from the harddrive.
+	 */
 	public void readFromFile() {
 
 		try {
